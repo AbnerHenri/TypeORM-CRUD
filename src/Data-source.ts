fd71d7ff444f6baaa3e7f7454d8f206f5a19b8ca
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-12 01:27:59 -0300

    Create DataSource and Initialize Server

---
+++ src/Data-source.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config' 
+import 'reflect-metadata'
+import { DataSource } from "typeorm"
+
+const myPort = process.env.DB_PORT as number | undefined
+
+const appDataSource = new DataSource({
+    type : "postgres",
+    host : process.env.DB_HOST,
+    port : myPort,
+    username : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+})
+
+export default appDataSource;
--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,16 @@
-import 'reflect-metadata'
 import express from 'express'
+import appDataSource from './Data-source'
 
-const app = express()
+appDataSource.initialize().then(()=>{ 
+        const app = express()
 
-app.listen(3333, ()=>{ console.log('Running') })
+        app.use(express.json())
+        app.get('/', (req,res)=>{
+            return res.json('Hello World')
+        })
+
+        return app.listen(process.env.PORT, ()=>{
+            console.log('Server Running')
+        })
+    })
+
